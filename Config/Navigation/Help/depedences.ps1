d5badd9475d4a2470e268dbd45e3d707ac07e119
Josue Aguiar <josueaguiar.sc@gmail.com>
2026-07-26 17:08:14 -0300

    [style] A cor do texto foi modificada - Antes: Branco/Padrão - Agora: Cinza

--- Config/Navigation/Help/depedences.ps1
+++ Config/Navigation/Help/depedences.ps1
@@ -1,8 +1,8 @@
     Write-Host "" <##>
-Write-Host "    Windows PowerShell 5.1 nao e suportado"
+Write-Host "    Windows PowerShell 5.1 nao e suportado" -ForegroundColor Gray
     Write-Host "" <##>
     Start-Sleep -Seconds 1
     Write-Host "" <##>
-Write-Host "    Utilize o PowerShell 7 para prosseguir"
+Write-Host "    Utilize o PowerShell 7 para prosseguir" -ForegroundColor Gray
     Write-Host "" <##>
 Set-Location    $HOME
