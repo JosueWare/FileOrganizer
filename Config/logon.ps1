8d7902eb65b2d62ca8309752184776ed8cfc86b6
Josue Aguiar <josueaguiar.sc@gmail.com>
2026-07-30 22:41:17 -0300

    [refactor]

--- Config/logon.ps1
+++ Config/logon.ps1
@@ -1,19 +1,15 @@
 # Logon
 
-    <#--[VAR]--#>
-
-        $main_file = Resolve-Path ".\Config\Navigation\main.ps1"
-
-        $dep_file = Resolve-Path ".\Config\Navigation\Help\depedences.ps1"
+    # ENV
+    $main_file = Resolve-Path ".\Config\Navigation\main.ps1"
+    $dep_file = Resolve-Path ".\Config\Navigation\Help\depedences.ps1"
 
 # Init
 
     if (Get-Command "pwsh.exe" -ErrorAction SilentlyContinue) {
-
         Start-Process -FilePath "pwsh.exe" -ArgumentList "-NoProfile", "-NoExit", "-Command $main_file" -Verb RunAs
     }
 
-    else {
-
-        Start-Process -FilePath "powershell.exe" -ArgumentList "-NoProfile", "-NoExit", "-Command $dep_file" -Verb RunAs
-    }
+        else {
+            Start-Process -FilePath "powershell.exe" -ArgumentList "-NoProfile", "-NoExit", "-Command $dep_file" -Verb RunAs
+        }
