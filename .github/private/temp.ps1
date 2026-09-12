310bd7bcfc96f9d80fa926e4f14709ff5a21da7f
Josue Aguiar <josueaguiar.sc@gmail.com>
2026-09-12 16:49:48 -0300

    added
    private docs

---
+++ .github/private/temp.ps1
@@ -0,0 +1,5 @@
+New-Item (Join-Path -Path "$folderMyDocuments" "FileOrganizer") -ItemType Directory
+
+New-Item (Join-Path -Path "$folderMyPictures" "FileOrganizer") -ItemType Directory
+
+New-Item (Join-Path -Path "$folderMyVideos" "FileOrganizer") -ItemType Directory
