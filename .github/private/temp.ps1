2108618296e850d287a28c92952abbaee22c0b64
Josue Aguiar <josueaguiar.sc@gmail.com>
2026-09-18 04:24:46 -0300

    docs: Removido a pasta '.github'

--- .github/private/temp.ps1
+++
@@ -1,5 +0,0 @@
-New-Item (Join-Path -Path "$folderMyDocuments" "FileOrganizer") -ItemType Directory
-
-New-Item (Join-Path -Path "$folderMyPictures" "FileOrganizer") -ItemType Directory
-
-New-Item (Join-Path -Path "$folderMyVideos" "FileOrganizer") -ItemType Directory
