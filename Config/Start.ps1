503bb6ad1cdd6d8f38f43879ca3a2ac37a844c19
Josue Aguiar <josueaguiar.sc@gmail.com>
2026-09-18 04:43:48 -0300

    fix

--- Config/Start.ps1
+++ Config/Start.ps1
@@ -4,10 +4,13 @@
     $MainFile = Resolve-Path (Join-Path -Path "Config" -ChildPath @("Main", "Menu.ps1"))
     $DepeFile = Resolve-Path (Join-Path -Path "Config" -ChildPath @("Main", "Depedences", "dpPwsh.ps1"))
 
+    $MainScript = Resolve-Path (Join-Path -Path "Config" -ChildPath "Main" | Join-Path -ChildPath "Menu.ps1")
+    $Depedences = Resolve-Path (Join-Path -Path "Config" -ChildPath "Main" | Join-Path -Path "Depedences" -ChildPath "dpPwsh.ps1")
+
 # Init
 if (Get-Command "pwsh.exe" -ErrorAction SilentlyContinue) {
-    Start-Process -FilePath "pwsh.exe" -ArgumentList "-NoProfile", "-NoExit", "-Command & $MainFile"
+    Start-Process -FilePath "pwsh.exe" -ArgumentList "-NoProfile", "-NoExit", "-Command & $MainScript"
 }
     else {
-        Start-Process -FilePath "powershell.exe" -ArgumentList "-NoProfile", "-NoExit", "-Command & $DepeFile"
+        Start-Process -FilePath "powershell.exe" -ArgumentList "-NoProfile", "-NoExit", "-Command & $Depedences"
     }
