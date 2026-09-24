3768e18818e5dbafdfe22657a4285408e349b141
Josue Aguiar <josueaguiar.sc@gmail.com>
2026-09-24 00:25:31 -0300

    testing

--- Config/Start.ps1
+++ Config/Start.ps1
@@ -1,18 +1,22 @@
-# Logon
+# ENV
 
-    # ENV
+    # "Menu.ps1"
+    $fullpath = Join-Path -Path "Config" -ChildPath "Main"
+    $fullpath = Join-Path -ChildPath "Menu.ps1"
 
-        # Full Path
-        $fullpath_Depedences = Join-Path -Path "Config" -ChildPath "Main"
-        $fullpath_Depedences = Join-Path -Path $fullpath_Depedences -ChildPath "Depedences"
-        $fullpath_Depedences = Join-Path -Path $fullpath_Depedences -ChildPath "dpPwsh.ps1"
+    $MainScript = Resolve-Path $fullpath
 
-    $MainScript = Resolve-Path (Join-Path -Path "Config" -ChildPath "Main" | Join-Path -ChildPath "Menu.ps1")
+    # Depedences "dpPwsh.ps1"
+    $fullpath = Join-Path -Path "Config" -ChildPath "Main"
+    $fullpath = Join-Path -Path "Depedences" -ChildPath "dpPwsh.ps1"
 
-# Init
-if (Get-Command "pwsh.exe" -ErrorAction SilentlyContinue) {
-    Start-Process -FilePath "pwsh.exe" -ArgumentList "-NoProfile", "-NoExit", "-Command & $MainScript"
+    $Depedences = Resolve-Path $fullpath
+
+# Start
+
+if (Get-Command "pwsh" -ErrorAction SilentlyContinue) {
+    Start-Process -FilePath "pwsh.exe" -ArgumentList "-NoProfile", "-NoExit", "Command & $MainScript" -Verb RunAs
 }
     else {
-        Start-Process -FilePath "powershell.exe" -ArgumentList "-NoProfile", "-NoExit", "-Command & $fullpath_Depedences"
+        Start-Process -FilePath "powershell.exe" -ArgumentList "-NoProfile", "-NoExit", "Command & $Depedences"
     }
