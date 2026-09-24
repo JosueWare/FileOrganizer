3def1043057e5a9c7af9900ade9c654861c67053
Josue Aguiar <josueaguiar.sc@gmail.com>
2026-09-24 00:35:15 -0300

    fixed

--- Config/Start.ps1
+++ Config/Start.ps1
@@ -2,21 +2,22 @@
 
     # "Menu.ps1"
     $fullpath = Join-Path -Path "Config" -ChildPath "Main"
-    $fullpath = Join-Path -ChildPath "Menu.ps1"
+    $fullpath = Join-Path -Path $fullpath -ChildPath "Menu.ps1"
 
-    $MainScript = Resolve-Path $fullpath
+    $MainScript = $fullpath
 
     # Depedences "dpPwsh.ps1"
     $fullpath = Join-Path -Path "Config" -ChildPath "Main"
-    $fullpath = Join-Path -Path "Depedences" -ChildPath "dpPwsh.ps1"
+    $fullpath = Join-Path -Path $fullpath -ChildPath "Depedences"
+    $fullpath = Join-Path -Path $fullpath -ChildPath "dpPwsh.ps1"
 
-    $Depedences = Resolve-Path $fullpath
+    $Depedences = $fullpath
 
 # Start
 
 if (Get-Command "pwsh" -ErrorAction SilentlyContinue) {
-    Start-Process -FilePath "pwsh.exe" -ArgumentList "-NoProfile", "-NoExit", "Command & $MainScript" -Verb RunAs
+    Start-Process -FilePath "pwsh.exe" -ArgumentList "-NoProfile", "-NoExit", "-Command & $MainScript" -Verb RunAs
 }
     else {
-        Start-Process -FilePath "powershell.exe" -ArgumentList "-NoProfile", "-NoExit", "Command & $Depedences"
+        Start-Process -FilePath "powershell.exe" -ArgumentList "-NoProfile", "-NoExit", "-Command & $Depedences"
     }
