90140bfa680969f995a66c741df608feaacb2fe1
Josue Aguiar <josueaguiar.sc@gmail.com>
2026-09-24 00:03:17 -0300

    feat

--- Config/Scripts/MoveItems.ps1
+++ Config/Scripts/MoveItems.ps1
@@ -15,25 +15,32 @@ Write-Host "    Movendo.."
 
     # Move-Items
 
-        # Documentos
+        # Documents
         $listed_GenericDocuments = @("*.txt", "*.md")
 
         foreach ($list in $listed_GenericDocuments) {
             Move-Item -Path "Media\$list" -Destination $target_MyDocs
         }
 
-        # Imagens
+        # Pictures
         $listed_Pictures = @("*.png", "*.jpg", "*.jpeg", "*.gif", "*.bmp", "*.svg", "*.webp", "*.avif", "*.apng", "*.raw")
 
         foreach ($list in $listed_Pictures) {
             Move-Item -Path "Media\$list" -Destination $target_MyPics
         }
 
-        # Vídeos
+        # Videos
         $listed_Videos = @("*.mp4", "*.mkv")
 
         foreach ($list in $listed_Videos) {
             Move-Item -Path "Media\$list" -Destination $target_MyVids
+        }
+
+        # Musics
+        $listed_Musics = @("*.mp3", "*.wav")
+
+        foreach ($list in $listed_Musics) {
+            Move-Item -Path "Media\$list" -Destination $target_MyMsc
         }
 
 
